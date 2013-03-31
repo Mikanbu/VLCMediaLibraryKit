5f0b823c0201a81f04f17206c87ba2e59f11c3ad
Felix Paul Kühne <fkuehne@videolan.org>
2013-04-01 01:33:37 +0200

    External: updated 3 year old TouchXML snapshot to current HEAD

--- External/TouchXML/Source/CXMLNode_PrivateExtensions.m
+++ External/TouchXML/Source/CXMLNode_PrivateExtensions.m
@@ -3,36 +3,118 @@
 //  TouchCode
 //
 //  Created by Jonathan Wight on 03/07/08.
-//  Copyright 2008 toxicsoftware.com. All rights reserved.
+//  Copyright 2011 toxicsoftware.com. All rights reserved.
 //
-//  Permission is hereby granted, free of charge, to any person
-//  obtaining a copy of this software and associated documentation
-//  files (the "Software"), to deal in the Software without
-//  restriction, including without limitation the rights to use,
-//  copy, modify, merge, publish, distribute, sublicense, and/or sell
-//  copies of the Software, and to permit persons to whom the
-//  Software is furnished to do so, subject to the following
-//  conditions:
+//  Redistribution and use in source and binary forms, with or without modification, are
+//  permitted provided that the following conditions are met:
 //
-//  The above copyright notice and this permission notice shall be
-//  included in all copies or substantial portions of the Software.
+//     1. Redistributions of source code must retain the above copyright notice, this list of
+//        conditions and the following disclaimer.
 //
-//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-//  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
-//  OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-//  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-//  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-//  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-//  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-//  OTHER DEALINGS IN THE SOFTWARE.
+//     2. Redistributions in binary form must reproduce the above copyright notice, this list
+//        of conditions and the following disclaimer in the documentation and/or other materials
+//        provided with the distribution.
 //
+//  THIS SOFTWARE IS PROVIDED BY TOXICSOFTWARE.COM ``AS IS'' AND ANY EXPRESS OR IMPLIED
+//  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+//  FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL TOXICSOFTWARE.COM OR
+//  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+//  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+//  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+//  ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+//  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+//  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+//  The views and conclusions contained in the software and documentation are those of the
+//  authors and should not be interpreted as representing official policies, either expressed
+//  or implied, of toxicsoftware.com.
 
 #import "CXMLNode_PrivateExtensions.h"
 
 #import "CXMLElement.h"
 #import "CXMLDocument_PrivateExtensions.h"
 
-@implementation CXMLNode (dd)
+@implementation CXMLNode (CXMLNode_PrivateExtensions)
 
+- (id)initWithLibXMLNode:(xmlNodePtr)inLibXMLNode freeOnDealloc:(BOOL)infreeOnDealloc
+{
+if (inLibXMLNode == NULL)
+	return nil;
+
+if ((self = [super init]) != NULL)
+	{
+	_node = inLibXMLNode;
+	_freeNodeOnRelease = infreeOnDealloc;
+	}
+return(self);
+}
+
++ (id)nodeWithLibXMLNode:(xmlNodePtr)inLibXMLNode freeOnDealloc:(BOOL)infreeOnDealloc
+{
+// TODO more checking.
+if (inLibXMLNode == NULL)
+	return nil;
+
+if (inLibXMLNode->_private)
+	return(inLibXMLNode->_private);
+
+Class theClass = [CXMLNode class];
+switch (inLibXMLNode->type)
+	{
+	case XML_ELEMENT_NODE:
+		theClass = [CXMLElement class];
+		break;
+	case XML_DOCUMENT_NODE:
+		theClass = [CXMLDocument class];
+		break;
+	case XML_ATTRIBUTE_NODE:
+	case XML_TEXT_NODE:
+	case XML_CDATA_SECTION_NODE:
+	case XML_COMMENT_NODE:
+		break;
+	default:
+		NSAssert1(NO, @"TODO Unhandled type (%d).", inLibXMLNode->type);
+		return(NULL);
+	}
+
+CXMLNode *theNode = [[[theClass alloc] initWithLibXMLNode:inLibXMLNode freeOnDealloc:infreeOnDealloc] autorelease];
+
+
+if (inLibXMLNode->doc != NULL)
+	{
+	CXMLDocument *theXMLDocument = inLibXMLNode->doc->_private;
+	if (theXMLDocument != NULL)
+		{
+		NSAssert([theXMLDocument isKindOfClass:[CXMLDocument class]] == YES, @"TODO");
+
+		[[theXMLDocument nodePool] addObject:theNode];
+
+		theNode->_node->_private = theNode;
+		}
+	}
+return(theNode);
+}
+
+- (xmlNodePtr)node
+{
+return(_node);
+}
+
+- (void)invalidate;
+    {
+    if (_node)
+        {
+        if (_node->_private == self)
+            _node->_private = NULL;
+
+        if (_freeNodeOnRelease)
+            {
+            xmlUnlinkNode(_node);
+            xmlFreeNode(_node);
+            }
+
+        _node = NULL;
+        }
+    }
 
 @end
