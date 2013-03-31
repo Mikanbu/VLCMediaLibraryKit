0ebe668674f267ac00892ef5805c81435d9e648a
Felix Paul Kühne <fkuehne@videolan.org>
2013-03-31 17:28:14 +0200

    MediaLibraryKit: following the concordant decision by the library's authors, Pierre d'Herbemont and Felix Paul Kühne, relicense all code from GPL v2 to LGPL v2.1

--- Headers/Public/MediaLibraryKit.h
+++ Headers/Public/MediaLibraryKit.h
@@ -3,7 +3,7 @@
  *****************************************************************************
  * Copyright (C) 2013 Felix Paul Kühne
  *
- * Authors: Felix Paul Kühne
+ * Authors: Felix Paul Kühne <fkuehne # videolan.org>
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by
