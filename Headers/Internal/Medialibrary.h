ed0c6915331663d5192b87da9470704441236c58
Soomin Lee <thehungrybu@gmail.com>
2017-07-20 01:38:55 +0200

    Medialibrary: Add internal medialibrary header

---
+++ Headers/Internal/Medialibrary.h
@@ -0,0 +1,44 @@
+/*****************************************************************************
+ * Medialibrary.h
+ * MediaLibraryKit
+ *****************************************************************************
+ * Copyright (C) 2010-2017 VLC authors and VideoLAN
+ * $Id$
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
+
+
+#ifndef Medialibrary_h
+# define Medialibrary_h
+
+// medialibrary
+# include <medialibrary/IAlbum.h>
+# include <medialibrary/IArtist.h>
+# include <medialibrary/IGenre.h>
+# include <medialibrary/IPlaylist.h>
+# include <medialibrary/IFolder.h>
+# include <medialibrary/Types.h>
+# include <medialibrary/IDeviceLister.h>
+# include <medialibrary/IMedia.h>
+# include <medialibrary/IMediaLibrary.h>
+
+// devicelister
+# include <medialibrary/IDeviceLister.h>
+
+// utils
+# include <medialibrary/Types.h>
+
+#endif /* Medialibrary_h */
