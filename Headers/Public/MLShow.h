c2876e8e21e10dc844908ac07b3fd39511d7b55a
Soomin Lee <thehungrybu@gmail.com>
2017-07-27 18:20:48 +0200

    MLShow: Cleanup

--- Headers/Public/MLShow.h
+++ Headers/Public/MLShow.h
@@ -1,12 +1,9 @@
 /*****************************************************************************
  * MLShow.h
- * Lunettes
+ * MediaLibraryKit
  *****************************************************************************
- * Copyright (C) 2010 Pierre d'Herbemont
- * Copyright (C) 2010-2013 VLC authors and VideoLAN
+ * Copyright (C) 2010-2017 VLC authors and VideoLAN
  * $Id$
- *
- * Authors: Pierre d'Herbemont <pdherbemont # videolan.org>
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by
@@ -23,36 +20,10 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
  *****************************************************************************/
 
-#import <CoreData/CoreData.h>
-
 @class MLShowEpisode;
 
 @interface MLShow : NSObject
 
-+ (NSArray *)allShows;
-+ (MLShow *)showWithName:(NSString *)name;
-
-@property (nonatomic, strong) NSString *theTVDBID;
-@property (nonatomic, strong) NSString *shortSummary;
-@property (nonatomic, strong) NSString *artworkURL;
-@property (nonatomic, strong) NSString *name;
-@property (nonatomic, strong) NSString *releaseYear;
-@property (nonatomic, strong) NSNumber *lastSyncDate;
-@property (nonatomic, strong) NSSet *episodes;
-@property (weak, nonatomic, readonly) NSArray *sortedEpisodes;
-@property (nonatomic, strong, readonly) NSSet *unreadEpisodes;
-
-- (void)addEpisode:(MLShowEpisode*)episode;
-- (void)removeEpisode:(MLShowEpisode*)episode;
-- (void)removeEpisodeWithSeasonNumber:(NSNumber *)seasonNumber andEpisodeNumber:(NSNumber *)episodeNumber;
 
 @end
-
-//
-//@interface MLShow   (CoreDataGeneratedAccessors)
-//- (void)addEpisodesObject:(NSManagedObject *)value;
-//- (void)removeEpisodesObject:(NSManagedObject *)value;
-//- (void)addEpisodes:(NSSet *)value;
-//- (void)removeEpisodes:(NSSet *)value;
-//@end
 
--- Sources/MLShow.m
+++ Sources/MLShow.m
@@ -1,13 +1,9 @@
 /*****************************************************************************
  * MLShow.m
- * Lunettes
+ * MediaLibraryKit
  *****************************************************************************
- * Copyright (C) 2010 Pierre d'Herbemont
- * Copyright (C) 2010-2015 VLC authors and VideoLAN
+ * Copyright (C) 2010-2017 VLC authors and VideoLAN
  * $Id$
- *
- * Authors: Pierre d'Herbemont <pdherbemont # videolan.org>
- *          Felix Paul Kühne <fkuehne # videolan.org>
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by
@@ -29,135 +25,5 @@
 #import "MLMediaLibrary.h"
 
 @implementation MLShow
-
-+ (NSArray *)allShows
-{
-    NSFetchRequest *request = [[NSFetchRequest alloc] init];
-    NSManagedObjectContext *moc = [[MLMediaLibrary sharedMediaLibrary] managedObjectContext];
-    if (!moc || moc.persistentStoreCoordinator == nil)
-        return [NSArray array];
-    NSEntityDescription *entity = [NSEntityDescription entityForName:@"Show" inManagedObjectContext:moc];
-    [request setEntity:entity];
-
-    NSSortDescriptor *descriptor = [[NSSortDescriptor alloc] initWithKey:@"name" ascending:NO];
-    [request setSortDescriptors:@[descriptor]];
-
-    NSArray *shows = [moc executeFetchRequest:request error:nil];
-
-    return shows;
-}
-
-//
-//+ (MLShow *)showWithName:(NSString *)name
-//{
-////    NSFetchRequest *request = [[MLMediaLibrary sharedMediaLibrary] fetchRequestForEntity:@"Show"];
-//    if (!request)
-//        return nil;
-//    [request setPredicate:[NSPredicate predicateWithFormat:@"name == %@", name]];
-//
-//    NSManagedObjectContext *moc = [[MLMediaLibrary sharedMediaLibrary] managedObjectContext];
-//    if (!moc)
-//        return nil;
-//
-//    NSArray *dbResults = [moc executeFetchRequest:request error:nil];
-//    NSAssert(dbResults, @"Can't execute fetch request");
-//
-//    if ([dbResults count] <= 0)
-//        return nil;
-//
-//    return dbResults[0];
-//}
-
-
-@dynamic theTVDBID;
-@dynamic shortSummary;
-@dynamic artworkURL;
-@dynamic name;
-@dynamic lastSyncDate;
-@dynamic releaseYear;
-@dynamic episodes;
-
-- (NSSet *)unreadEpisodes
-{
-    NSArray *episodes = [[self valueForKey:@"episodes"] allObjects];
-    NSMutableSet *set = [NSMutableSet set];
-    NSUInteger count = episodes.count;
-    for (NSUInteger x = 0; x < count; x++) {
-        NSSet *files = [episodes[x] valueForKey:@"files"];
-        for (id file in files) {
-            if ([[file valueForKey:@"unread"] boolValue]) {
-                [set addObject:episodes[x]];
-                break;
-            }
-        }
-    }
-    return set;
-}
-
-- (NSArray *)sortedEpisodes
-{
-    NSArray *episodes = [[self valueForKey:@"episodes"] allObjects];
-
-    NSSortDescriptor *seasonDescriptor =
-    [[NSSortDescriptor alloc] initWithKey:@"seasonNumber"
-                                ascending:YES
-                                 selector:@selector(compare:)];
-    NSSortDescriptor *episodesDescriptor =
-    [[NSSortDescriptor alloc] initWithKey:@"episodeNumber"
-                                ascending:YES
-                                 selector:@selector(compare:)];
-    NSArray *descriptors = @[seasonDescriptor, episodesDescriptor];
-    return [episodes sortedArrayUsingDescriptors:descriptors];
-}
-
-- (void)addEpisode:(MLShowEpisode*)episode
-{
-    if (!episode)
-        return;
-
-    NSMutableSet *episodes = [self mutableSetValueForKey:@"episodes"];
-    [episodes addObject:episode];
-
-    [self willChangeValueForKey:@"episodes"];
-    [self setValue:episodes forKey:@"episodes"];
-    [self didChangeValueForKey:@"episodes"];
-}
-
-- (void)removeEpisode:(MLShowEpisode*)episode
-{
-    if (!episode)
-        return;
-
-    NSMutableSet *episodes = [self mutableSetValueForKey:@"episodes"];
-
-    [episodes removeObject:episode];
-
-    [self willChangeValueForKey:@"episodes"];
-    [self setValue:episodes forKey:@"episodes"];
-    [self didChangeValueForKey:@"episodes"];
-}
-
-- (void)removeEpisodeWithSeasonNumber:(NSNumber *)seasonNumber andEpisodeNumber:(NSNumber *)episodeNumber
-{
-    NSMutableSet *episodes = [self mutableSetValueForKey:@"episodes"];
-    MLShowEpisode *episode = nil;
-    if (seasonNumber && episodeNumber) {
-        for (MLShowEpisode *episodeIter in episodes) {
-            if ([episodeIter.seasonNumber intValue] == [seasonNumber intValue] &&
-                [episodeIter.episodeNumber intValue] == [episodeNumber intValue]) {
-                episode = episodeIter;
-                break;
-            }
-        }
-    }
-    if (!episode)
-        return;
-
-    [episodes removeObject:episode];
-
-    [self willChangeValueForKey:@"episodes"];
-    [self setValue:episodes forKey:@"episodes"];
-    [self didChangeValueForKey:@"episodes"];
-}
 
 @end
